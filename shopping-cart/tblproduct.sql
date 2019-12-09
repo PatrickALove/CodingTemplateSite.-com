e8daa4e467c2ef8561b45bc0ccec0b6699b789c3
Patrick Love <p.andrew.love@gmail.com>
2019-12-08 23:46:16 -0500

    All files dealing with Shopping Cart

---
+++ shopping-cart/tblproduct.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 06, 2019 at 06:12 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tblproduct`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblproduct`
+--
+
+CREATE TABLE `tblproduct` (
+  `id` int(8) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `image` text NOT NULL,
+  `price` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblproduct`
+--
+
+INSERT INTO `tblproduct` (`id`, `name`, `code`, `image`, `price`) VALUES
+(1, 'Stack Script Interpreter', 'SSI01', './shopping-cart/product-images/SSI.png', 10.00),
+(2, 'Comment Remover Program', 'CRP01', './shopping-cart/product-images/com-rem-pro.png', 45.33),
+(3, 'Ruby CSV Parser', 'csvPar', './shopping-cart/product-images/rubyCSV.png', 29.99),
+(4, 'GTCC Template Site', 'GTCCTemp01', './shopping-cart/product-images/GTCC-template-site.png', 200.00),
+(5, 'CSS Resources', 'CSSR01', './shopping-cart/product-images/CSS-resources.png', 75.99),
+(6, 'Python Video Tutorials (Full Course)', 'PVT01', './shopping-cart/product-images/python-vid-tuts.png', 399.99);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblproduct`
+--
+ALTER TABLE `tblproduct`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `product_code` (`code`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblproduct`
+--
+ALTER TABLE `tblproduct`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
